b687681d59093112218b806af430e3768612df81
miholeus <me@miholeus.com>
2018-02-12 15:51:46 +0300

    one entry point :zap:

--- web/app.php
+++ web/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../vendor/autoload.php';
@@ -7,7 +8,14 @@ if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
 }
 
-$kernel = new AppKernel('prod', false);
+$env = getenv('SYMFONY_ENV') ?: 'prod';
+$debug = getenv('SYMFONY_DEBUG') === '1' && $env != 'prod';
+
+if ($debug) {
+    Debug::enable();
+}
+
+$kernel = new AppKernel($env, $debug);
 if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }
