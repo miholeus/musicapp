5357e9de9f5756d3ca5152db33075d399b695b9f
miholeus <me@miholeus.com>
2018-02-12 23:23:21 +0300

    introducing core bundle :sparkles:

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new CoreBundle\CoreBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
             new FOS\RestBundle\FOSRestBundle(),
