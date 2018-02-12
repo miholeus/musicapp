8bb25c512c42172497a40b1436bd1057fd3b39e4
miholeus <me@miholeus.com>
2018-02-13 02:35:12 +0300

    listeners :construction:

--- src/CoreBundle/CoreBundle.php
+++ src/CoreBundle/CoreBundle.php
@@ -2,8 +2,16 @@
 
 namespace CoreBundle;
 
+use CoreBundle\DependencyInjection\Compiler\DoctrineEntityListenerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CoreBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new DoctrineEntityListenerPass());
+    }
 }
---
+++ src/CoreBundle/DependencyInjection/Compiler/DoctrineEntityListenerPass.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CoreBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class DoctrineEntityListenerPass
+ */
+class DoctrineEntityListenerPass implements CompilerPassInterface
+{
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $definition = $container->getDefinition('core.doctrine.entity_listener_resolver');
+        $services = $container->findTaggedServiceIds('doctrine.orm.entity_listener');
+
+        foreach ($services as $service => $attributes) {
+            $definition->addMethodCall(
+                'addMapping',
+                array($container->getDefinition($service)->getClass(), $service)
+            );
+        }
+    }
+}
